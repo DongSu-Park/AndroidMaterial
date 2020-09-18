0ada188b1bf98c66f14ab64bcf715759981cbcc3
FloreErin <floreerin@gmail.com>
2020-09-18 14:33:12 +0900

    List Fragment Material Add

--- app/src/main/java/com/example/materialtutorial/navigation/ListFragment.kt
+++ app/src/main/java/com/example/materialtutorial/navigation/ListFragment.kt
@@ -3,15 +3,40 @@ package com.example.materialtutorial.navigation
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.materialtutorial.R
+import com.example.materialtutorial.navigation.model.ItemDTO
 import kotlinx.android.synthetic.main.fragment_list.view.*
+import kotlinx.android.synthetic.main.item_listview.view.*
 
 class ListFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_list, container, false)
 
-        val mCollapsingToolbarLayout = view.findViewById<View>(R.id.layout_CollapsingToolbar)
+        view.layout_RecyclerView.adapter = ListAdapter()
+        view.layout_RecyclerView.layoutManager = LinearLayoutManager(activity)
 
         return view
     }
+
+    inner class ListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var items : MutableList<ItemDTO> = mutableListOf(ItemDTO(1),
+            ItemDTO(2),ItemDTO(3),ItemDTO(4),ItemDTO(5),ItemDTO(6),ItemDTO(7),ItemDTO(8),ItemDTO(9),ItemDTO(10))
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_listview, parent, false)
+            return ItemViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val viewHolder = (holder as ItemViewHolder).itemView
+            viewHolder.item_list_text.text = "item = ${items[position].itemID}"
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        inner class ItemViewHolder(view : View) : RecyclerView.ViewHolder(view)
+    }
+
 }
